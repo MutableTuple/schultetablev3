128d727d6da191d7c6e1ae16887714bbdf10508d
Yogesh Vishwakarma <mutabletuple39@gmail.com>
2025-07-05 12:12:38 +0530

    PWA and local play support

---
+++ app/head.js
@@ -0,0 +1,11 @@
+// app/head.js
+export default function Head() {
+  return (
+    <>
+      <title>Schulte Table</title>
+      <meta name="theme-color" content="#ffde59" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/icons/icon-192.png" />
+    </>
+  );
+}
